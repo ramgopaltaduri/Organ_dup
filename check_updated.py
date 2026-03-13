e59e893113c8d3b019a9d76e94b964b4e4af7c33
ramgopaltaduri <143475076+ramgopaltaduri@users.noreply.github.com>
2026-03-13 11:15:33 +0530

    Delete check_updated.py

--- check_updated.py
+++
@@ -1,8 +0,0 @@
-from pptx import Presentation
-prs=Presentation('final_ppt_updated.pptx')
-for i,slide in enumerate(prs.slides):
-    print('Slide', i+1)
-    for shape in slide.shapes:
-        if hasattr(shape,'text') and shape.text.strip():
-            print(shape.text)
-    print('----')
